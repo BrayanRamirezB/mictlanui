f6fb2ab2096f63c01726469a543e1038d9b34ec1
RamirezBB <brbrayan1945@gmail.com>
2025-05-06 15:04:09 -0600

    set eager loading to false for search data to improve performance

--- src/content/searchData.ts
+++ src/content/searchData.ts
@@ -1,7 +1,7 @@
 // src/pages/searchData.ts
 export const pages = Object.entries(
   import.meta.glob<{ frontmatter: { name: string } }>('/src/content/**/*.mdx', {
-    eager: true
+    eager: false
   })
 ).map(([path, mod]) => ({
   title: mod.frontmatter?.name || path.split('/').pop()?.replace('.mdx', ''),
