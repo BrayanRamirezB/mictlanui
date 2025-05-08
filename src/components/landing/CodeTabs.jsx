5221bbba6149b6018c97156b2fa88677422a1f98
RamirezBB <brbrayan1945@gmail.com>
2025-05-08 15:14:54 -0600

    feat: change remove code highlighter

--- src/components/landing/CodeTabs.jsx
+++ src/components/landing/CodeTabs.jsx
@@ -1,21 +1,12 @@
-import { useMemo } from 'react'
 import Tab from '@/components/react/Tabs/Tab.jsx'
 import Tabs from '@/components/react/Tabs/Tabs.jsx'
 import CodeBlock from '@/components/landing/CodeBlock'
-import { highlightOneDark } from '@/utils/highlight'
 
 const CodeTabs = ({ tabs }) => {
-  const highlightedTabs = useMemo(() => {
-    return tabs.map((tab) => ({
-      ...tab,
-      highlightedCode: highlightOneDark(tab.code)
-    }))
-  }, [tabs])
-
   return (
     <div className='max-w-3xl w-auto mx-auto p-2 not-prose'>
       <Tabs variant='light'>
-        {highlightedTabs.map((tab) => (
+        {tabs.map((tab) => (
           <Tab key={tab.label} label={tab.label}>
             <CodeBlock>
               <pre
@@ -23,12 +14,7 @@ const CodeTabs = ({ tabs }) => {
                 aria-label={`Code snippet for ${tab.label}`}
                 role='region'
               >
-                <code
-                  className='font-mono'
-                  dangerouslySetInnerHTML={{
-                    __html: tab.highlightedCode
-                  }}
-                ></code>
+                <code className='font-mono'>{tab.code}</code>
               </pre>
             </CodeBlock>
           </Tab>
