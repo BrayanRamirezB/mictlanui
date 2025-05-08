5684479832fa157522d42413b7c1e4f55822b8a0
RamirezBB <brbrayan1945@gmail.com>
2025-05-07 21:38:07 -0600

    feat: enhance accessibility by adding role and aria attributes to Skeleton component

--- src/components/ts/Skeleton/Skeleton.tsx
+++ src/components/ts/Skeleton/Skeleton.tsx
@@ -13,9 +13,15 @@ const Skeleton: FC<SkeletonProps> = ({ isLoaded, children, className }) => {
         isLoaded ? '' : `backdrop-blur-sm shadow-lg animate-pulse ${className}`
       }`}
       data-loaded={isLoaded}
+      role='status'
+      aria-busy={!isLoaded}
+      aria-live='polite'
     >
       {!isLoaded && (
-        <div className='absolute inset-0 transform bg-zinc-700/30 dark:bg-gray-600 animate-pulse' />
+        <div
+          className='absolute inset-0 transform bg-zinc-700/30 dark:bg-gray-600 animate-pulse'
+          aria-hidden='true'
+        />
       )}
       <div className={isLoaded ? '' : 'opacity-0'}>{children}</div>
     </div>
