24faf4349c9fa6be795bfc5fbf85d325e4ca3302
RamirezBB <brbrayan1945@gmail.com>
2025-05-24 14:58:53 -0600

    feat: Optimization and improvement of the Spinner typescript component

--- src/components/ts/Spinner.tsx
+++ src/components/ts/Spinner.tsx
@@ -1,65 +1,71 @@
-import { type FC } from 'react'
+import { memo, type FC } from 'react'
 
-interface SpinnerProps {
+type Variant = keyof typeof SpinnerConfig.variants
+type Size = keyof typeof SpinnerConfig.sizes
+type Color = keyof typeof SpinnerConfig.colors
+
+export interface SpinnerProps {
   label?: string
-  variant?: 'default' | 'light'
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-  color?: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
-  textColor?:
-    | 'default'
-    | 'primary'
-    | 'secondary'
-    | 'success'
-    | 'warning'
-    | 'danger'
+  variant?: Variant
+  size?: Size
+  color?: Color
+  textColor?: Color
 }
+
+const SpinnerConfig = {
+  variants: {
+    default: 'backdrop-blur-sm shadow-lg',
+    light: ''
+  },
+  sizes: {
+    xs: 'size-4',
+    sm: 'size-6',
+    md: 'size-8',
+    lg: 'size-10',
+    xl: 'size-12'
+  },
+  colors: {
+    default: {
+      firstCircle: 'border-zinc-500',
+      secondCircle: 'border-zinc-400',
+      text: 'text-gray-800 dark:text-gray-300'
+    },
+    primary: {
+      firstCircle: 'border-blue-500',
+      secondCircle: 'border-blue-300',
+      text: 'text-blue-800 dark:text-blue-600'
+    },
+    secondary: {
+      firstCircle: 'border-indigo-500',
+      secondCircle: 'border-indigo-300',
+      text: 'text-indigo-800 dark:text-indigo-600'
+    },
+    success: {
+      firstCircle: 'border-green-500',
+      secondCircle: 'border-green-300',
+      text: 'text-green-800 dark:text-green-600'
+    },
+    warning: {
+      firstCircle: 'border-yellow-500',
+      secondCircle: 'border-yellow-300',
+      text: 'text-yellow-800 dark:text-yellow-600'
+    },
+    danger: {
+      firstCircle: 'border-red-500',
+      secondCircle: 'border-red-300',
+      text: 'text-red-800 dark:text-red-500'
+    }
+  }
+} as const
 
 const Spinner: FC<SpinnerProps> = ({
   label,
   variant = 'default',
   size = 'md',
   color = 'default',
-  textColor = 'default'
+  textColor = color
 }) => {
-  const variants = {
-    default: 'backdrop-blur-sm shadow-lg',
-    light: ''
-  }
-
-  const sizes = {
-    xs: 'size-4',
-    sm: 'size-6',
-    md: 'size-8',
-    lg: 'size-10',
-    xl: 'size-12'
-  }
-
-  const firstCircleColors = {
-    default: 'border-zinc-500',
-    primary: 'border-blue-500',
-    secondary: 'border-indigo-500',
-    success: 'border-green-500',
-    warning: 'border-yellow-500',
-    danger: 'border-red-500'
-  }
-
-  const secondCircleColors = {
-    default: 'border-zinc-400',
-    primary: 'border-blue-300',
-    secondary: 'border-indigo-300',
-    success: 'border-green-300',
-    warning: 'border-yellow-300',
-    danger: 'border-red-300'
-  }
-
-  const textColors = {
-    default: 'text-gray-800 dark:text-gray-300',
-    primary: 'text-blue-800 dark:text-blue-600',
-    secondary: 'text-indigo-800 dark:text-indigo-600',
-    success: 'text-green-800 dark:text-green-600',
-    warning: 'text-yellow-800 dark:text-yellow-600',
-    danger: 'text-red-800 dark:text-red-500'
-  }
+  const { variants, sizes, colors } = SpinnerConfig
 
   return (
     <div
@@ -68,23 +74,21 @@ const Spinner: FC<SpinnerProps> = ({
       aria-live='polite'
       aria-label={label || 'Loading'}
     >
-      <div className='relative'>
+      <div
+        className={`relative rounded-full ${variants[variant]} ${sizes[size]}`}
+      >
         <div
-          className={`relative rounded-full ${variants[variant]} ${sizes[size]}`}
-        >
-          <div
-            className={`absolute w-full h-full rounded-full border-4 border-t-transparent border-r-transparent border-l-transparent ${firstCircleColors[color]} animate-spin`}
-          ></div>
-          <div
-            className={`absolute w-full h-full rounded-full border-4 border-t-transparent border-b-transparent border-l-transparent ${secondCircleColors[color]} animate-spin`}
-          ></div>
-        </div>
+          className={`absolute w-full h-full rounded-full border-4 border-t-transparent border-r-transparent border-l-transparent ${colors[color].firstCircle} animate-spin`}
+        />
+        <div
+          className={`absolute w-full h-full rounded-full border-4 border-t-transparent border-b-transparent border-l-transparent ${colors[color].secondCircle} animate-spin`}
+        />
       </div>
       {label && (
-        <span className={`text-sm ${textColors[textColor]}`}>{label}</span>
+        <span className={`text-sm ${colors[textColor].text}`}>{label}</span>
       )}
     </div>
   )
 }
 
-export default Spinner
+export default memo(Spinner)
